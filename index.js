70a2b37ed5508d69cf02d8b8244a092f6d404d9d
ellisju37073 <67281488+ellisju37073@users.noreply.github.com>
2021-10-28 14:13:14 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let request = require('request');
+let apiKey = '4d48a58bc7c265b825f824b9ed5c20b4';
+
+let city = 'seattle';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+  }
+});
